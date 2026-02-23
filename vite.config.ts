a773558d40a9c9353252ed1163969e5a24716b16
TenkyuChimata <tenkyuchimata@gmail.com>
2026-02-23 20:10:39 +0800

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/ssv", // 部署基础路径。若需要部署在abc.com/ssv，则需要将base修改为"/ssv"。
+  base: "/", // 部署基础路径。若需要部署在abc.com/ssv，则需要将base修改为"/ssv"。
   build: {
     outDir: "dist", // 打包输出目录
 
